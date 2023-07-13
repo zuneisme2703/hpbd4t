1160f421056158a4bbc4852d6b751370d25a74cd
zuneisme2703 <121237340+zuneisme2703@users.noreply.github.com>
2023-07-13 08:35:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const contentLetterSrart_actived = "Hãy nói gì đó ở đây trước khi người ấy mở bức thư nhé." //Lời mở đầu cho bức thư
+const contentLetterSrart_actived = "Nay sinh nhật bạn 4T á nên mình có vài lời chúc mún gửi đến bạn nè!" //Lời mở đầu cho bức thư
 const mainContentLetter = "Gửi lời nhắn nhủ đến người bạn bạn yêu thương." //Nội dung của bức thư
 
 // Gắn 1 đường link ảnh bất kì
